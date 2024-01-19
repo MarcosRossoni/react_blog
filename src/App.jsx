1e5b94e933cb69e9cae06ea294b219a68e1f2e49
marcos <marcosrossoni9@gmail.com>
2024-01-19 17:34:02 -0300

    inicio de desenvolvimento de blog

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {Outlet} from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+
+function App() {
+
+    return (
+        <div className="App">
+            <NavBar/>
+            <div className="container">
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
+
+export default App
